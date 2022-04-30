09b290c82f258b2638a1842209fb4830e4a702e6
Moses Karani <karanim594@gmail.com>
2022-04-30 17:40:49 +0300

    added more functions but no full routing

--- app/main/views.py
+++ app/main/views.py
@@ -6,4 +6,26 @@ from . import main
 #Views 
 @main.route('/')
 def home():
+    '''
+    View home page that displays/returns the home page of the app
+    '''
+
     return render_template('home.html')
+
+
+@main.route('/')
+def index():
+    '''
+    View index page that displays/returns news sources
+    '''
+
+    return render_template('index.html')
+
+
+@main.route('/')
+def about():
+    '''
+    View about page that displays/returns the details of articles
+    '''
+
+    return render_template('about.html')
