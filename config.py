7fe833e4abdfbe9fa871fe2a5fa574a566b62179
Moses Karani <karanim594@gmail.com>
2022-05-01 16:23:16 +0300

    update the config.py with base url & article url

--- config.py
+++ config.py
@@ -2,13 +2,19 @@
 
 from distutils.command.config import config
 
+from instance.config import NEWS_API_KEY
+
 
 class Config:
     '''
     General configureation parent class
     '''
-    
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    NEWS_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY = 'c8595be1853149d6a6c3547f36c268d0'
 
+
+    
 class ProdConfig(Config):
     '''
     Production configuration child class
