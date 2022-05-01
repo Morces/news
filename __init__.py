c6e0d46bf2ab763c0295c659e01d45fc7a383ecf
Moses Karani <karanim594@gmail.com>
2022-05-01 19:09:41 +0300

    updated init file

--- __init__.py
+++ __init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from config import config_options
-from distutils.command.config import config
+from app.requests import configure_request 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
+
+    # Registering the Blueprint
+    from main import main as blue_print
+    app.register_blueprint(blue_print)
+
+    # Setting up configurations
+    from requests import configure_request
+    configure_request(app) 
+
+    return app 
