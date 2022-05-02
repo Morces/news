01d6e49c52ad00d74dda1bcbf4bc16be87b9f577
Moses Karani <karanim594@gmail.com>
2022-05-02 15:14:19 +0300

    Patch:fixed missing module error

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
+
+from app.requests import configure_request
 from config import config_options
-from app.requests import configure_request 
+
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
 
     # Registering the Blueprint
-    from main import main as blue_print
+    from .main import main as blue_print
     app.register_blueprint(blue_print)
 
     # Setting up configurations
-    from requests import configure_request
+    from .requests import configure_request
     configure_request(app) 
 
     return app 
--- manage.py
+++ manage.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3.8
+
+from flask_script import Manager, Server
+from app import create_app
+
+#creating app instance
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+if __name__ == '__main__':
+    manager.run()
 
